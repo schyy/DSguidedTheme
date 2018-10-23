65871820d33661ed7cb1eac326ce2476b1d6579f
schyy <b.kosinski@azupedia.com>
2018-10-23 16:17:23 +0200

    0.105

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.price').html($('.price').html().replace(*, ));
+  $('.price').html($('.price').html().replace("*","" ));
   console.log("hello");
 });
 console.log("hello2");
