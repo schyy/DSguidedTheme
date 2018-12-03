a3b5cdad5fb44b44ddf233a3168b94d4b8a627d1
schyy <schrott@azupedia.com>
2018-12-03 02:32:08 +0100

    0.140

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-  var lengthOfSelect = $('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length
+  var lengthOfSelect = $('div.col-xs-12.col-md-6.col-lg-4.variation-select').length
   alert("length = " + lengthOfSelect);
 
-  if(!$('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length){
+  if(!$('.col-xs-12.col-md-6.col-lg-4.variation-select').length){
 		$('.add-to-basket-container').html("AUSVERKAUFT");
 	}
   else{
-    $('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').find('select').nextAll('option:first:enabled').val();
+    $('.col-xs-12.col-md-6.col-lg-4.variation-select').find('select').nextAll('option:first:enabled').val();
   }
-  $('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').find('select').nextAll('option:disabled').val().append("-ausverkauft");
+  $('div.variation-select').find('select').nextAll('option:disabled').val().append("-ausverkauft");
   //$('option:disabled').val().append("-ausverkauft");
 });
