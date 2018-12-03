711531086d54c9eb545049820763ae6bc1000caf
schyy <schrott@azupedia.com>
2018-12-03 01:40:19 +0100

    0.135

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  if($('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').show()){
+  if($('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length()){
     alert("select ist da");
   }
-  if(!$('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').show()){
+  if(!$('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length()){
     alert("select ist NICHT da");
   }
-  $('.add-to-basket-container').remove();
+  $('.add-to-basket-container').remove().append("AUSVERKAUFT !");
 
   if(!$('.variation-select').show()){
 		$('.add-to-basket-container').remove();
