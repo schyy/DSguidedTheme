f0d8addc34cdeb4ea2c3e80dd6ac0d75e82f3d0a
schyy <schrott@azupedia.com>
2018-12-03 01:01:51 +0100

    0.129

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,2 +1,8 @@
 $(document).ready(function(){
+  if(!$('.m-b-1').find('.variation-select')){
+		$('.m-y-1').remove();
+	}
+  $('select option:disabled').value("ausverkauft");
+    //$(this).value = $(this).value + " - Ausverkauft";
+  }
 });
