d3b96f45af3f29fdc55791c60cb849c8dc7709d5
schyy <schrott@azupedia.com>
2018-12-03 01:55:03 +0100

    0.137

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  if($('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length{
+  if($('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length){
     alert("select ist da");
   }
-  if(!$('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length{
+  if(!$('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length){
     alert("select ist NICHT da");
   }
   $('.add-to-basket-container').remove().append("AUSVERKAUFT !");
