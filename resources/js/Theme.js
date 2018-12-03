096c399bebecb8078090ad6abf71a8d78272c7c9
schyy <schrott@azupedia.com>
2018-12-03 03:15:35 +0100

    0.143

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
 		$('.add-to-basket-container').html("<div class='ausverkauft'>AUSVERKAUFT</div>");
 	}
   else{
-    $('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select > option:enabled:first').val();
+    $('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select > option:enabled:first').remove();
   }
-  $('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select > option:disabled:first').val("AUSVERKAUFT");
+  $('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select > option:disabled:first').text("AUSVERKAUFT");
   //$('option:disabled').val().append("-ausverkauft");
 });
