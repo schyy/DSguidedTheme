6c33dd9b9365afb57366a986b266bbbb61bbc6f8
schyy <b.kosinski@azupedia.com>
2018-12-03 11:05:06 +0100

    0.155

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -7,6 +7,18 @@ $(document).ready(function(){
   else{
     //$('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select').find("option:not(:disabled):first").prop('selected',true);
   }
+  var disabledText, isSelected = false;
+  $('.variation-select > input-unit > select > option').each(function(){
+    console.log(this.text);
+    if(this.is(':disabled')){
+      disabledText = this.text();
+      this.text(disabledText += "-SOLD");
+    }
+    else if(!isSelected){
+      this.val();
+      isSelected = true;
+    }
+  });
   //$('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select').nextAll("option:disabled").text("-SOLD");
   //$('option:disabled').val().append("-ausverkauft");
 });
