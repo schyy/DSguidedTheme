be06d880fd404df166870c002fe062671faaba7e
schyy <schrott@azupedia.com>
2018-12-03 02:21:07 +0100

    0.139

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-  alert("length = "$('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length);
+  var lengthOfSelect = $('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length
+  alert("length = " + lengthOfSelect);
 
   if(!$('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').length){
-		$('.add-to-basket-container').remove().parent().append("AUSVERKAUFT");
+		$('.add-to-basket-container').html("AUSVERKAUFT");
 	}
-  $('option:disabled').val().append("-ausverkauft");
+  else{
+    $('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').find('select').nextAll('option:first:enabled').val();
+  }
+  $('.col-xs-12 .col-md-6 .col-lg-4 .variation-select').find('select').nextAll('option:disabled').val().append("-ausverkauft");
+  //$('option:disabled').val().append("-ausverkauft");
 });
