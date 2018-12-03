ba4cae92f0da9d76202d4ce5f48f171aa685b924
schyy <schrott@azupedia.com>
2018-12-03 01:27:44 +0100

    0.133

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  if($('.variation-select').show()){
+    alert("select ist da");
+  }
+  if(!$('.variation-select').show()){
+    alert("select ist NICHT da");
+  }
+  $('.add-to-basket-container').remove();
+
   if(!$('.variation-select').show()){
 		$('.add-to-basket-container').remove();
 	}
