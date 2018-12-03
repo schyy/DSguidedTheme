1a05e76ad7e94d8b47690bb7592af3bfe9435a9e
schyy <schrott@azupedia.com>
2018-12-03 04:13:09 +0100

    0.147

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   var lengthOfSelect = $('div.col-xs-12.col-md-6.col-lg-4.variation-select').length
 
   if($('.col-xs-12.col-md-6.col-lg-4.variation-select').length<1){
-		$('.add-to-basket-container').html("<div class='ausverkauft'>AUSVERKAUFT</div>");
+		$('.add-to-basket-container').html("<div class='ausverkauft'><h1>AUSVERKAUFT</h1></div>");
 	}
   else{
     $('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select').find("option:not(:disabled):eq(0)").prop('selected',true);
   }
-  $('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select').find("option:is(:disabled)").text().append("-SOLD");
+  $('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select').nextAll("option:disabled").text().append("-SOLD");
   //$('option:disabled').val().append("-ausverkauft");
 });
