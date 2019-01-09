e402ea4e2db9aaa8cfb5fecbcb709872fd59dbd3
schyy <b.kosinski@azupedia.com>
2019-01-09 13:52:06 +0100

    0.165

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -21,4 +21,21 @@ $(document).ready(function(){
   });
   //$('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select').nextAll("option:disabled").text("-SOLD");
   //$('option:disabled').val().append("-ausverkauft");
+
+  $('.panel-body').hide();
+	$('.panel-heading').css('cursor','pointer');
+	$('.glyphicon').css('font-size','12px');
+	$('.glyphicon').css('margin-right','10px');
+	$('.panel-heading').click(function(e){
+		if($(this).next().is(":visible")){
+			$(this).next().slideUp();
+			$(this).find('.glyphicon-chevron-down').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
+		}
+		else{
+			$('.panel-body').slideUp().prev().find('.glyphicon-chevron-down').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
+			$(this).next().slideDown();
+			$(this).find('.glyphicon-chevron-right').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-down');
+		}
+	});
+  
 });
