7811b77bcf88b635836f355d9b6f43e56bb3bca6
schyy <6203834+schyy@users.noreply.github.com>
2019-09-10 11:43:24 +0200

    0.170

--- resources/js/Theme.js
+++ resources/js/Theme.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   var lengthOfSelect = $('div.col-xs-12.col-md-6.col-lg-4.variation-select').length
 
-  if($('.col-xs-12.col-md-6.col-lg-4.variation-select').length<1){
+  /*if($('.col-xs-12.col-md-6.col-lg-4.variation-select').length<1){
 		$('.add-to-basket-container').html("<div class='ausverkauft'><h1>AUSVERKAUFT</h1></div>");
 	}
   else{
     //$('.col-xs-12.col-md-6.col-lg-4.variation-select > .input-unit > select').find("option:not(:disabled):first").prop('selected',true);
-  }
+  }*/
   var disabledText, isSelected = false;
   $('.variation-select > .input-unit > select > option').each(function(){
     console.log($(this).text);
